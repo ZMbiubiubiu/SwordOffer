1a0556fb0fd390a7491c25b108d1d1c3d23f2409
张猛 <390047718@qq.com>
2019-06-17 21:29:59 +0800

    44:加上不同的排序算法

--- 44s.cpp
+++ 44s.cpp
@@ -7,14 +7,17 @@ public:
     bool IsContinuous( vector<int> numbers ) {
         // 边界检查
         if (numbers.size() < 5) return false;
-        SelectSort(numbers); // 用选择排序算法排序
+        // SelectSort(numbers); // 用选择排序算法排序
         // sort(numbers.begin(), numbers.end());
+        // BubbleSort(numbers);
+        // InsertSort(numbers);
+        QuickSort(numbers);
         // 癞子的数量
         int king = 0;
         for(int i=0; i<numbers.size(); i++) {
             if (numbers[i] == 0) {
                 king++;
-            } else {
+                 } else {
                 break;
             }
         }
@@ -30,7 +33,63 @@ public:
         }
         return king >= weak;
     }
+    
+    void QuickSort(vector<int>& numbers) {
+        // 边界检查
+        if (numbers.size() <= 1) return;
+        int low = 0;
+        int high = numbers.size() - 1;
+        Sort(numbers, low, high);
+    }
+    void Sort(vector<int>& numbers, int low, int high) {
+        if (low >= high) return;
+        int mid = Partition(numbers, low, high); 
+        Sort(numbers, low, mid-1);
+        Sort(numbers, mid+1, high);
+    }
+    int Partition(vector<int>& numbers, int low, int high) {
+        int flag = numbers[low];
+        int left = low+1;
+        int right = high;
+        while (1) {
+            while (numbers[left] <= flag && left < high) 
+                left++;
+            while (numbers[right] >= flag && right > low) 
+                right--;
+            if (left >= right) {
+                break;
+            }
+            swap(numbers, left, right);
+        }
+        swap(numbers, low, right);
+        return right;
+    }
+    
+    void InsertSort(vector<int>& numbers) {
+        // 边界检查
+        if (numbers.size() <=1 ) return ;
+        int i,j;
+        for (i=1; i<numbers.size(); i++) {
+            int tmp = numbers[i];
+            for (j=i; j>0 && numbers[j-1] > tmp; j--) {
+                numbers[j] = numbers[j-1];
+            }
+            numbers[j] = tmp;
+        }
+    }
     void BubbleSort(vector<int>& numbers) {
+        // 边界检查
+        if (numbers.size() <= 1) return;
+        bool sorted = false;
+        while (!sorted) {
+            sorted = true;
+            for(int i=0; i<numbers.size()-1; i++) {
+                if (numbers[i] > numbers[i+1]) {
+                    sorted = false;
+                    swap(numbers, i, i+1);
+                }
+            }
+        }
         for(int i=0; i<numbers.size()-1; i++) {
             cout << i;
         }
